9c23293673988ebfc7f10e2a60bc6dc026032dd4
Rebecca Herrera <133678924+reherr@users.noreply.github.com>
2024-06-03 16:05:40 +0000

    view templates

---
+++ app/controllers/dice_controller.rb
@@ -0,0 +1,19 @@
+class DiceController < ApplicationController
+  def roll
+    @num_dice = params.fetch("number_of_dice").to_i
+    @sides = params.fetch("how_many_sides").to_i
+  
+    @rolls = []
+  
+    @num_dice.times do
+      die = rand(1..@sides)
+  
+      @rolls.push(die)
+    end
+  render({ :template => "game_template/flexible"})
+  end
+
+  def rules
+    render({ :template => "game_templates/homepage"})
+  end
+end
